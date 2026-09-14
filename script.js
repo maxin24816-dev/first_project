25dbdc32cef24bcf0abf8fc58ebe37c31dbbdb69
Maksym Kapustin <kmaks4541@gmail.com>
2026-09-14 19:00:19 +0300

    Starting doing main, added univs for math and updated js

--- script.js
+++ script.js
@@ -1,17 +1,34 @@
-const to_main = document.getElementById("to_main");
-const to_all = document.getElementById("to_all");
-const to_about = document.getElementById("to_about");
-const to_sources = document.getElementById("to_sources");
+function find(id){
+    return document.getElementById(id);
+}
+function giveRedirect(obj, page){
+    if (!obj){
+        return
+    }
+    obj.addEventListener("click", () => {
+        window.location.href = page;
+    })
+}
 
-to_main.addEventListener("click", () => {
-    window.location.href = "index.html";
-})
-to_all.addEventListener("click", () => {
-    window.location.href = "every_place.html";
-})
-to_about.addEventListener("click", () => {
-    window.location.href = "about.html";
-})
-to_sources.addEventListener("click", () => {
-    window.location.href = "sources.html";
-})
+const to_main = find("to_main");
+const to_all = document.querySelectorAll(".to_all");
+const to_about = find("to_about");
+const to_sources = find("to_sources");
+
+const to_math = find("to_math");
+const to_it = find("to_it");
+const to_talk = find("to_talk");
+const to_management = find("to_management");
+const to_filology = find("to_filology");
+
+giveRedirect(to_main, "index.html");
+for (let i = 0;i<to_all.length;i++){
+    giveRedirect(to_all[i], "every_place.html");
+}
+giveRedirect(to_about, "about.html");
+giveRedirect(to_sources, "sources.html");
+giveRedirect(to_math, "math.html");
+giveRedirect(to_it, "it.html");
+giveRedirect(to_talk, "talk.html");
+giveRedirect(to_management, "management.html");
+giveRedirect(to_filology, "filology.html");
